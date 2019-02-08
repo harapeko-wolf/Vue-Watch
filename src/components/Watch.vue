57e8bb9b54fea21fdb538a4ecdf4c08ebbdd12f5
harapeko-wolf <32303847+harapeko-wolf@users.noreply.github.com>
2019-02-08 16:57:31 +0900

    Initial commit

---
+++ src/components/Watch.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="">
+    <p>{{ message }}</p>
+    <p><input type="text" v-model="message" /></p>
+    <pre>{{$data}}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Watch",
+  data() {
+    return {
+      message: "Vue.js"
+    };
+  },
+  watch: {
+    message: function(newValue, oldValue) {
+      console.log("new: %s, old: %s", newValue, oldValue);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+pre {
+  text-align: left;
+}
+</style>
